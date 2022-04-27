5f424cb9a555fa3a83dbb20f5a98a7374886426a
sigristarisa <arisasigrist.ch@gmail.com>
2022-04-27 13:15:37 +0200

    created the basic post request

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const todoUl = document.querySelector("#todo-list");
+const submit = document.querySelector("form");
 
+// get data
 fetch("http://localhost:3000/todos")
   .then((res) => res.json())
   .then((todos) => {
@@ -9,3 +11,20 @@ fetch("http://localhost:3000/todos")
       todoUl.append(todoLi);
     });
   });
+
+submit.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const newTodo = e.target[0].value;
+  const opts = {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ title: newTodo }),
+  };
+  fetch("http://localhost:3000/todos", opts)
+    .then((res) => res.json())
+    .then((todos) => {
+      const todoLi = document.createElement("li");
+      todoLi.innerText = todos.title;
+      todoUl.append(todoLi);
+    });
+});
