cec7271b42c4061114b7fed744d3dc6ec10de9de
sigristarisa <arisasigrist.ch@gmail.com>
2022-04-27 16:30:17 +0200

    finished second extension

--- src/createTodoLi.js
+++ src/createTodoLi.js
@@ -2,10 +2,12 @@ const createTodoLi = (todo) => {
   const todoUl = document.querySelector("#todo-list");
   const todoLi = document.createElement("li");
   const completeBtn = document.createElement("button");
+  const deleteBtn = document.createElement("button");
 
   todoLi.innerText = todo.title;
   completeBtn.innerText = "✔︎";
-  todoLi.append(completeBtn);
+  deleteBtn.innerText = "delete";
+  todoLi.append(completeBtn, deleteBtn);
   todoUl.append(todoLi);
 
   completeBtn.addEventListener("click", () => {
@@ -20,6 +22,16 @@ const createTodoLi = (todo) => {
       .then((res) => res.json())
       .then((todo) => {
         todoLi.innerHTML = `<s>${todo.title}</s>`;
+      });
+  });
+
+  deleteBtn.addEventListener("click", () => {
+    fetch(`http://localhost:3000/todos/${todo.id}`, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then((todo) => {
+        todoLi.remove();
       });
   });
 };
