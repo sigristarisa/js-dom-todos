44718d6e26e3850d00f30674beb592645a910bd3
sigristarisa <arisasigrist.ch@gmail.com>
2022-04-27 17:58:13 +0200

    added styles

--- src/createTodoLi.js
+++ src/createTodoLi.js
@@ -4,13 +4,16 @@ import deleteRequest from "./deleteRequest.js";
 const createTodoLi = (todo) => {
   const todoUl = document.querySelector("#todo-list");
   const todoLi = document.createElement("li");
+  const btnContainer = document.createElement("div");
   const completeBtn = document.createElement("button");
   const deleteBtn = document.createElement("button");
 
   todoLi.innerText = todo.title;
   completeBtn.innerText = "✔︎";
-  deleteBtn.innerText = "delete";
-  todoLi.append(completeBtn, deleteBtn);
+  deleteBtn.innerText = "×";
+  btnContainer.setAttribute("class", "btnContainer");
+  btnContainer.append(completeBtn, deleteBtn);
+  todoLi.append(btnContainer);
   todoUl.append(todoLi);
 
   completeBtn.addEventListener("click", () => {
